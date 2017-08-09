7765a05374aa4c19950b126239c366a880a0aec6
IndraNyo <frx99indra@sina.com>
2017-08-09 23:30:38 +0800

    vue

---
+++ src/pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Hello <span>World</span></p>
+    </div>
+</template>
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    // p{
+    //     color: #f90;
+    //     span{
+    //         color: #f20;
+    //     }
+    // }
+</style>
